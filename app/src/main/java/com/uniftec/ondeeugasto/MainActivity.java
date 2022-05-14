68e5bb9d1e61a91c3a3a3519c943f6a7aad88d1b
Henrique Wagner <wagner.ike@gmail.com>
2022-05-14 13:05:55 -0300

    criacao tela login

--- app/src/main/java/com/uniftec/ondeeugasto/MainActivity.java
+++ app/src/main/java/com/uniftec/ondeeugasto/MainActivity.java
@@ -1,6 +1,18 @@
 package com.uniftec.ondeeugasto;
+import android.app.Fragment;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.app.*;
 
 import android.os.Bundle;
 
@@ -9,6 +21,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
+
+
+
+
+
+
     }
 }
