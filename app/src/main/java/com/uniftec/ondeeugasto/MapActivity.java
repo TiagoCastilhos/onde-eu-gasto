7666450fea4e3fd95a00de80935c742bce402362
TiagoCastilhos <tiagocastilhos66@gmail.com>
2022-05-14 12:42:05 -0300

    Criada a navbar e componente de navegação

--- app/src/main/java/com/uniftec/ondeeugasto/MapActivity.java
+++ app/src/main/java/com/uniftec/ondeeugasto/MapActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.uniftec.ondeeugasto.navigation.Navigator;
+
 public class MapActivity extends AppCompatActivity {
+
+    private Navigator navigator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        this.navigator = new Navigator(MapActivity.this);
     }
 }
